1bfe361989e7fc32d4505d1a18ef45aa0d210400
sohel_sutriya <sohelsutriya143@gmail.com>
2020-05-05 21:20:00 +0530

    udated

--- src/main/java/com/cg/obs/FundTransferApplication.java
+++ src/main/java/com/cg/obs/FundTransferApplication.java
@@ -2,12 +2,20 @@ package com.cg.obs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FundTransferApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundTransferApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate createTemplate() {
+		return new RestTemplate();
+	}
 }
